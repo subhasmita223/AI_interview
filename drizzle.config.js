1c32e9792d367f9c3055dd0392b55700f708bba2
subhasmita223 <subhasmitaswain1619@gmail.com>
2025-06-29 23:36:11 +0530

    Remove sensitive drizzle.config.js from Git history

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-// import { defineConfig } from "drizzle-kit";
-/** @type {import('drizzle-kit').Config}   */
-export default ({
-  schema: "./utils/schema.js",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: "postgresql://neondb_owner:npg_ncM03QGEBwfq@ep-purple-darkness-a81u6zbw-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
-  }
-});
